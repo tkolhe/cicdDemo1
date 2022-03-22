dd6405ca202369b469217020c55fb318d216d7eb
Tanuja Subhash Kolhe <tkolhe@altimetrik.com>
2022-03-22 16:08:05 +0530

    Local changes

---
+++ src/main/java/com/demo/app/CicdDemo3Application.java
@@ -0,0 +1,13 @@
+package com.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemo3Application.class, args);
+	}
+
+}
